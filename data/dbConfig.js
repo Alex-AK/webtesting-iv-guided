5daa880003cb6e7c4194450ebe1a1b827d389f10
Kingaa6 <Kingaa6@gmail.com>
2019-03-07 11:02:25 -0800

    Update pg production deployment notes

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,4 +3,7 @@ const config = require('../knexfile.js');
 
 const dbEnv = process.env.DB_ENV || 'development';
 
+// go to heroku and add env variable : "DB_ENV = production"
+// then install knex CLI
+
 module.exports = knex(config[dbEnv]);
